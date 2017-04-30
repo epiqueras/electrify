09468cca2b721342ee33b02a9fc77d0029731c73
Enrique <epiquerass@gmail.com>
2017-04-29 21:28:09 -0700

    Added onDOMLoaded.

--- template/_src/_renderer/_index.js
+++ template/_src/_renderer/_index.js
@@ -1,13 +1,12 @@
 /* global window, document */
 import { ipcRenderer } from 'electron'
 
-// Load stylesheets
-import './styles/index.scss'
+document.addEventListener('DOMContentLoaded', () => {
+  // Request and set the theme
+  document.documentElement.classList = ipcRenderer.sendSync('getTheme')
 
-// Request and set the theme
-document.documentElement.classList = ipcRenderer.sendSync('getTheme')
-
-// Listen to theme changes from the main process
-ipcRenderer.on('themeChanges', (e, theme) => {
-  document.documentElement.classList = theme
+  // Listen to theme changes from the main process
+  ipcRenderer.on('themeChanges', (e, theme) => {
+    document.documentElement.classList = theme
+  })
 })
