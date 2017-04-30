3e21d0678c3e725a6b974ceb425cab8d4aadcd23
Enrique <epiquerass@gmail.com>
2017-04-30 02:11:50 -0700

    Made it path.sep agnostic.

--- src/start.js
+++ src/start.js
@@ -1,5 +1,5 @@
 import { statSync, mkdirSync, readdirSync, readFileSync, writeFileSync } from 'fs'
-import { join } from 'path'
+import { join, sep } from 'path'
 import getInput from './get-input'
 
 async function start() {
@@ -8,14 +8,14 @@ async function start() {
 
   function iterate(path, first) {
     if (statSync(path).isDirectory()) {
-      if (!first) mkdirSync(join(cwd, path.replace(/.*\/template\/_/, '').replace(/\/_/g, '/')))
+      if (!first) mkdirSync(join(cwd, path.replace(RegExp(`.*\\${sep}template\\${sep}_`), '').replace(RegExp(`\\${sep}_`, 'g'), sep)))
       const paths = readdirSync(path).filter(p => p[0] !== '.')
       for (const p of paths) iterate(join(path, p))
       return
     }
 
     const newFile = readFileSync(path, 'utf8').replace(/<% (\w+) %>/g, (m, p1) => input[p1])
-    writeFileSync(join(cwd, path.replace(/.*\/template\/_/, '').replace(/\/_/g, '/')), newFile)
+    writeFileSync(join(cwd, path.replace(RegExp(`.*\\${sep}template\\${sep}_`), '').replace(RegExp(`\\${sep}_`, 'g'), sep)), newFile)
   }
 
   iterate(join(__dirname, '../template/'), true)
